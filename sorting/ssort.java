113c0c603b9045424a253d13b4daf96d93823ff1
yagantikrishna <87084824+yagantikrishna@users.noreply.github.com>
2022-09-10 14:12:41 -0700

    Update ssort.java

--- sorting/ssort.java
+++ sorting/ssort.java
@@ -1,23 +1,24 @@
-import java.util.*;
-
+import java.util.*
 public class ssort {
     public void sort(int[] arr, ArrayList<Integer> ds, int index) {
-        if (arr.length == 1) {
-            System.out.println(ds);
-        }
         for (int i = 0; i < arr.length; i++) {
             int min = arr[index];
             if (arr[i] < min) {
                 min = arr[i];
+                index=i;
             }
-            index = i;
         }
         swap(arr, 0, index);
         ds.add(arr[0]);
-        for (int j = 1; j < arr.length; j++) {
-            arr[j - 1] = arr[j];
+        int[] arr2=new int[arr.length-1];
+        if(arr2.length<1){
+            System.out.println(ds);
+            return;
         }
-        sort(arr, ds, 0);
+        for (int j = 1; j < arr.length; j++) {
+            arr2[j - 1] = arr[j];
+        }
+        sort(arr2, ds, 0);
     }
 
     public static void swap(int[] arr, int index1, int index2) {
@@ -34,15 +35,16 @@ public class ssort {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] numbers = new int[10];
+        int[] numbers = new int[10000];
         ArrayList<Integer> ds = new ArrayList<>();
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rand.nextInt(100);
         }
         System.out.println("Before:");
         ssort s1 = new ssort();
-        s1.printArray(numbers);
+        //s1.printArray(numbers);
         System.out.println("\nAfter:");
         s1.sort(numbers, ds, 0);
+
     }
 }
